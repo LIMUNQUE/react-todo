5f44ae8ac302bc98fbbdf15feac731539d4f5c35
Joshua Cobos <satochiotake@gmail.com>
2022-03-24 21:31:23 -0500

    agregamos el sistema de search,complet y delete de TODOs

--- src/App.js
+++ src/App.js
@@ -13,29 +13,28 @@ const defaultTodos = [
   { text: 'LALALALAA', completed: false },
 ];
 
-
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue,setSearchValue]=React.useState('');
-  const completedTodos=todos.filter(todo=>todo.completed).length;
-  const totalTodos=todos.length;
+  const [searchValue,setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
 
-  let searchedTodos=[];
-  if(!searchValue>=1){
-    searchedTodos=defaultTodos;
+  let searchedTodos = [];
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos;
   }else{
     searchedTodos=todos.filter(todo=>
-    {const todoText=todo.text.toLowerCase();
-    const searchtext=searchValue.toLowerCase();
+    {const todoText = todo.text.toLowerCase();
+    const searchtext = searchValue.toLowerCase();
     return todoText.includes(searchtext);
   })
   }
 
   
-  const completeTodo=(text)=>{
-    const todoIndex=todos.findIndex(todo=>todo.text===text);
-    const newTodos=[...todos]
-    todos[todoIndex].completed=true;
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo=>todo.text===text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
     setTodos(newTodos);
   }
 
@@ -50,22 +49,22 @@ function App() {
     <React.Fragment>
       
       <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
+        total = {totalTodos}
+        completed = {completedTodos}
       />
       <TodoSearch 
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
       />
       
       <TodoList>
         {searchedTodos.map(todo => (
           <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
+            key = {todo.text}
+            text = {todo.text}
+            completed = {todo.completed}
+            onComplete = {() => completeTodo(todo.text)}
+            onDelete = {() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
-const onClickButton=(msg)=>{
+const onClickButton = (msg) => {
   alert("Aqui se debería abrir el modal");
   console.log(msg);
 };
   return (
-    <button onClick={()=>onClickButton("hi")} className="CreateTodoButton">+</button>
+    <button onClick = {() => onClickButton("hi")} className = "CreateTodoButton">+</button>
   );
 }
 
